2237e9c08014a9a83cafc5dd4e54fe65b14249a3
osaro imarhiagbe <osaroimarhiagbe@gmail.com>
2025-01-14 21:59:54 -0600

    new push

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      "nativewind/babel",
+      ["nativewind/babel"],
       [
         'module:react-native-dotenv',
         {
