71bef19f2ece8f6e46cbf786ee48c0c5c197a60d
osaro imarhiagbe <osaroimarhiagbe@gmail.com>
2025-01-31 12:47:02 -0600

    new push

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('@expo/metro-config');
-
 const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push('cjs');
+defaultConfig.resolver.sourceExts.push('cjs','ts','tsx');
 defaultConfig.resolver.assetExts.push('lottie');
 module.exports = defaultConfig;
